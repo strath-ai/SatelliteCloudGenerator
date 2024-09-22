d0787cbdc1b39a7976f89aa085f1430f3207eda6
mikonvergence <mikolaj.czerkawski@esa.int>
2024-09-22 10:40:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='satellite-cloud-generator',
-    version='0.3',    
-    description='A PyTorch-based tool for simulating clouds in satellite images.',
+    version='0.4',    
+    description='A PyTorch-based tool for simulating clouds in satellite images',
+    long_description='A PyTorch-based tool for simulating clouds in satellite images',
     url='https://github.com/strath-ai/SatelliteCloudGenerator',
     author='Mikolaj Czerkawski, Christos Tachtatzis',
     author_email="mikolaj.czerkawski@esa.int",
-    license='Apache 2.0',
     package_dir={"satellite_cloud_generator":"src"},
     install_requires=[
         "torch>=1.10.0",
